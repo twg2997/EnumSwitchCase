6c81a500c614be28f88ae3a13588d93961c91ea3
twg2997 <twg2997@email.vccs.edu>
2015-11-19 23:50:27 -0500

    PPPP

---
+++ evenDebug.java
@@ -0,0 +1,28 @@
+public class evenDebug {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int evens = 2;
+		
+		System.out.println("First 100 even-numbers:");
+		
+		do { System.out.print(evens + ", ");
+		  evens += 2;
+		}
+		
+		
+		while (evens <= 100); 
+		  
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
+
