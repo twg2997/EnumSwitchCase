d9229047f0d2050fcf4ec450d63fe10c7c157822
twg2997 <twg2997@email.vccs.edu>
2015-11-19 23:44:06 -0500

    AAAA

---
+++ oddDebug.java
@@ -0,0 +1,23 @@
+public class oddDebug {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int max = 100;
+
+		System.out.println("First 100 odd-numbers: ");
+
+		for (int i = 1; i <= max; i++) {
+
+			if (i % 2 != 0) {
+
+				System.out.print(i + ", ");
+
+			}
+
+		}
+
+	}
+
+}
+
